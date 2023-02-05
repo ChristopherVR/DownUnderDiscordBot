4c954aa924faf33a1f800cb620f6d2aa557a5d59
Christopher van Rooyen <chrisjvrooyen@gmail.com>
2023-02-05 18:07:01 +1100

    Update play.ts

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -223,7 +223,7 @@ export const Play: PlayerCommand = {
               });
               console.log('track played');
             } else {
-              await interaction.followUp('');
+              await interaction.followUp(localizedString('global:songAddedToQueue', { lng: interaction.locale, song }));
               console.log('There is already a track playing. Adding new one to the queue.');
             }
           }
