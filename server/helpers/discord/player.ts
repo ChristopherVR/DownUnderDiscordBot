8854caa0f79055ebe76f0f696f047cc0dde617cc
Christopher van Rooyen <chrisjvrooyen@gmail.com>
2025-10-08 19:04:21 +1000

    Resolved streaming issues.

--- server/helpers/discord/player.ts
+++ server/helpers/discord/player.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 import { Player } from 'discord-player';
 import { AttachmentExtractor } from '@discord-player/extractor';
 import { Client } from 'discord.js';
@@ -6,10 +5,10 @@ import { PlayerEventManager } from './playerEventManager';
 import { PlayerStateManager } from '../status/playerStateManager';
 import { WebSocketManager } from '../websocket';
 import { YoutubeiExtractor } from 'discord-player-youtubei';
+import { ClientType, Innertube } from 'youtubei.js';
+import { URL } from 'node:url';
 import { createLogger } from '../logger';
-import { spawn } from 'node:child_process';
-import { Readable } from 'node:stream';
-import Innertube from 'youtubei.js';
+import { Readable } from 'stream';
 
 let player: Player | null = null;
 let playerEventManager: PlayerEventManager | null = null;
@@ -38,49 +37,6 @@ export const getPlayerEventManager = (): PlayerEventManager => {
   return playerEventManager;
 };
 
-function ffmpegPcmFrom(input: Readable): Readable {
-  const ff = spawn(
-    process.env.FFMPEG_PATH ?? 'ffmpeg',
-    [
-      '-hide_banner',
-      '-loglevel',
-      'debug',
-      '-stats_period',
-      '0.5',
-      '-reconnect',
-      '1',
-      '-reconnect_streamed',
-      '1',
-      '-reconnect_delay_max',
-      '5',
-      '-rw_timeout',
-      '15000000',
-      '-i',
-      'pipe:0',
-      '-vn',
-      '-sn',
-      '-dn',
-      '-f',
-      's16le',
-      '-ar',
-      '48000',
-      '-ac',
-      '2',
-      'pipe:1',
-    ],
-    { stdio: ['pipe', 'pipe', 'pipe'] },
-  );
-
-  input.once('error', (err) => ff.stdin.destroy(err));
-  input.pipe(ff.stdin);
-
-  ff.stderr.on('data', (d) => console.debug('[ffmpeg]', String(d).trim()));
-  ff.on('error', (err) => console.error('[ffmpeg] error', err));
-  ff.on('close', (code) => console.debug('[ffmpeg] exited', code));
-
-  return ff.stdout as unknown as Readable;
-}
-
 export const initializePlayer = async (client: Client, wsManager?: WebSocketManager): Promise<Player> => {
   if (player) {
     return player;
@@ -89,7 +45,7 @@ export const initializePlayer = async (client: Client, wsManager?: WebSocketMana
   player = new Player(client);
 
   player.events.on('debug', (_queue, message) => {
-    // log.debug(`Debug: ${message} on guild ${_queue.guild.id}`);
+    log.debug(`Debug: ${message} on guild ${_queue.guild.id}`);
   });
   player.events.on('playerError', (queue, error) => {
     log.error(error);
@@ -112,40 +68,39 @@ export const initializePlayer = async (client: Client, wsManager?: WebSocketMana
     innertubeConfigRaw: {
       // Git issue - https://github.com/LuanRT/YouTube.js/issues/1043
       player_id: '0004de42',
+      client_type: 'WEB',
+      gl: 'ZA',
+      hl: 'en',
     },
-    cookie:
-      'APISID=VS9JpvhAVMoH4zhm/A-IEgpeicIett-elP; SAPISID=-ZMQZc_53sAiMYRd/A_266j80jHQkzm8TA; __Secure-1PAPISID=-ZMQZc_53sAiMYRd/A_266j80jHQkzm8TA; __Secure-3PAPISID=-ZMQZc_53sAiMYRd/A_266j80jHQkzm8TA; goodTube_autoplay=true; goodTube_uniqueUserStat=true; PREF=f6=40000000&tz=Australia.Brisbane&f7=100&repeat=NONE&f5=20000; SID=g.a0002Aj0mXJVX5sVTywGkahyp49MpFDuWWN9UIu5cX19TRPr5qE1unqz6TU4qjv9ufvJynzHzQACgYKAbsSARISFQHGX2MivPbRzqaixyiEorIeT7NSABoVAUF8yKo6TXAwsTndkagflgMyjOtB0076; SIDCC=AKEyXzUdzfKMpKqnRQy7t_JGCjT6OvorWi5rB9hMI477DNJSRmLjO17uLNVosYgRAZ0rN5W67o4',
-    generateWithPoToken: true,
-    ignoreSignInErrors: true,
-    overrideBridgeMode: 'yt',
+    streamOptions: { highWaterMark: 1 << 25 },
 
-    // createStream: async (track, extractor) => {
-    //   const id = track.id || new URL(track.url).searchParams.get('v') || track.url.split('/').pop();
+    createStream: async (track) => {
+      const url = track.url;
+      if (!url) throw new Error('Track has no URL');
 
-    //   try {
-    //     const t = await (
-    //       await Innertube.create()
-    //     ).download(id!, {
-    //       type: 'video+audio',
-    //       // quality: 'best',
-    //       client: 'WEB',
-    //     });
-    //   } catch (error) {
-    //     log.error('Error fetching video info from YouTube');
-    //   }
+      try {
+        const innerTube = await Innertube.create({
+          player_id: '0004de42',
+          client_type: ClientType.ANDROID,
+        });
 
-    //   const url = await extractor.innerTube.download(id!, {
-    //     type: 'video+audio',
-    //     quality: 'best',
-    //   });
+        const uarl = new URL(url);
 
-    //   return ffmpegPcmFrom(Readable.from(url));
-    // },
-    streamOptions: {
-      highWaterMark: 1 << 25,
-      debugStream: (msg: string) => log.error(`YT Stream: ${msg}`),
+        const vidId = uarl.searchParams.get('v');
+
+        const info = await innerTube.getBasicInfo(vidId!);
+
+        const stream = await info.download();
+
+        return Readable.from(stream);
+      } catch (err) {
+        log.error({ err, url }, 'Failed to fetch stream with this type, trying fallback');
+
+        throw err;
+      }
     },
   });
+
   // Initialize state manager with enhanced functionality
   playerStateManager = new PlayerStateManager(player);
 
